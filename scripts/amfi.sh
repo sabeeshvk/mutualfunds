45cca4c3589325b86570ba33512033aedd1e19f5
Sabeesh Velluvakkandy <sabeeshvk@gmail.com>
2021-10-21 21:49:40 +0530

    MF data

---
+++ scripts/amfi.sh
@@ -0,0 +1,20 @@
+
+cmd="date"
+cur_month=`$cmd | cut -d " " -f 2`
+cur_year=`$cmd | cut -d " " -f 6`
+echo $cur_month $cur_year
+for i in {1..240}
+do
+ cmd="date -j -v -$i"m
+ echo $cmd 
+ $cmd
+ prev_month=`$cmd | cut -d " " -f 2` 
+ prev_month_num=`$cmd +"%m"` 
+ prev_year=`$cmd | cut -d " " -f 6`
+ ccmd="curl  -o $prev_year-$prev_month_num  --request GET http://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?frmdt=01-$prev_month-$prev_year&todt=01-$cur_month-$cur_year"
+ echo $prev_month $year $prev_month_num
+ echo $ccmd
+ $ccmd
+ cur_month=$prev_month
+ cur_year=$prev_year
+done
