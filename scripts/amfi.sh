14b3603f33c9bb9a52cab4842fb3c5c13b6ac52c
Sabeesh Velluvakkandy <sabeeshvk@gmail.com>
2022-01-30 00:44:24 +0530

    AMFI HTTPS

--- scripts/amfi.sh
+++ scripts/amfi.sh
@@ -11,7 +11,7 @@ do
  prev_month=`$cmd | cut -d " " -f 2` 
  prev_month_num=`$cmd +"%m"` 
  prev_year=`$cmd | cut -d " " -f 6`
- ccmd="curl  -o ../navdata/monthly/$prev_year-$prev_month_num  --request GET http://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?frmdt=01-$prev_month-$prev_year&todt=01-$cur_month-$cur_year"
+ ccmd="curl  -o ../navdata/monthly/$prev_year-$prev_month_num  --request GET https://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?frmdt=01-$prev_month-$prev_year&todt=01-$cur_month-$cur_year"
  echo $prev_month $year $prev_month_num
  echo $ccmd
  $ccmd
